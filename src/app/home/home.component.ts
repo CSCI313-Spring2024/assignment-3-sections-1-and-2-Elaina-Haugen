57973ff1ced9e5899ea0cba482443bda8466e3d2
Elaina-Haugen <elaina.haugen@ndsu.edu>
2025-03-18 14:01:37 -0500

    milestone 11 added search feature

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,10 +13,20 @@ export class HomeComponent {
   
   housingLocationList: Housinglocation[] = [];
   housingService: HousingService = inject(HousingService);
-
+  filteredLocationList: Housinglocation[] = [];
   constructor(){
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList; 
   }
+  filterResults(text:string){ 
+    if (!text) { 
+      this.filteredLocationList = this.housingLocationList; 
+      return; 
+    } 
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) => 
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()), 
+    ); 
+  } 
   
 } 
   
